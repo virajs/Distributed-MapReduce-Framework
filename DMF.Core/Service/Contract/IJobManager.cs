27d0856d472bebc86a5617ce045b68c1675191ac
viraj.siriwardana <viraj2005@gmail.com>
2012-02-16 17:40:37 +1100

    Simply basic infrastructure.

---
+++ DMF.Core/Service/Contract/IJobManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMF.Core.Service.Contract
+{
+    public interface IJobManager
+    {
+        string RegisterJob(byte[] a);
+    }
+}

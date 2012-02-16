b2a69a7680b17ad0adc500b6e3656e8c0eb04cb5
viraj.siriwardana <viraj2005@gmail.com>
2012-02-16 11:40:59 +1100

    Initial solution setup.

---
+++ DMF.WindowsService.Master/Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace DMF.WindowsService.Master
+{
+    public partial class Master : ServiceBase
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
